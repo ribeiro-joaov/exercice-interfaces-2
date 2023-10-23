7a912b213f3f898fd9e18fba3793d16eb40ee716
ribeiro-joaov <ribeiro.joaov@gmail.com>
2023-10-23 17:37:32 -0300

    Initial commit

---
+++ src/services/ContractService.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class ContractService {
+
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months){
+        
+
+    }
+}
